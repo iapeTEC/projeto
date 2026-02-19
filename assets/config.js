93ae31182e2b8600367131dfceaad808ba1ab5e5
iapeTEC <bruno.souza4@eaportal.org>
2026-02-19 11:02:38 -0300

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 // Configuração do Front-end
 // Troque a URL abaixo se você implantar uma nova versão do Web App.
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxehjyYOlMvz6c2wr1zoRELbcl406askfVVNoLE5ScEUMlxR33aDo4UiarekuqIqIE__g/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxmT11Whtdb0YWb8sJ9XPonhUg6sIBg-fu0oLFqJparPAUAT2XQll3OFKizFSbp2usMmQ/exec",
   APP_NAME: "Bolsistas • Gestão",
   THEME_COLOR: "#0b1f3a"
 };
