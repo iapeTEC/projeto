6153877b87c95cc5b41fb5b77bdb72597375484a
iapeTEC <bruno.souza4@eaportal.org>
2026-02-24 15:58:22 -0300

    Add files via upload

---
+++ assets/config2.js
@@ -0,0 +1,7 @@
+// Configuração do Front-end
+// Troque a URL abaixo se você implantar uma nova versão do Web App.
+window.APP_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbxYqo3hdy6Pgl_gj0zY10RYNN70tgl1QMPoZVaCncSqNUpgT52W_QHPnd9_n3JXMIQA/exec",
+  APP_NAME: "Bolsistas • Gestão",
+  THEME_COLOR: "#0b1f3a"
+};
